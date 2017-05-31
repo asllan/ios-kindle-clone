9b346a907caec68e51faf773b13efe24d344cdf0
asllan <asllan77@gmail.com>
2017-05-30 20:25:19 -0400

    create book and page class

--- kindle-clone/ViewController.swift
+++ kindle-clone/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
+class Book {
+    let title: String
+    let author: String
+    let pages: [Page]
+    
+    init(title: String, author: String, pages: [Page]) {
+        self.title = title
+        self.author = author
+        self.pages = pages
+    }
+}
+
+class Page {
+    let number: Int
+    let text: String
+    
+    init(number: Int, text: String) {
+        self.number = number
+        self.text = text
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .red
+        
+        let page1 = Page(number: 1, text: "Text for the first page")
+        let page2 = Page(number: 2, text: "This is text for the second page")
+        
+//        print(page1.text)
+//        print(page2.text)
+        
+        let pages = [page1, page2]
+        
+        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
+//        print(book.title, book.author)
+//        print("Title: \(book.title)  Author: \(book.author)")
+        
+        let firstPage = book.pages[0].text
+        print(firstPage)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
