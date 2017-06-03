9a18426ae7e5b95366e44758ec53cbd24997d8a0
asllan <asllan77@gmail.com>
2017-06-02 20:34:27 -0400

    parse json object to book object

--- kindle-clone/ViewController.swift
+++ kindle-clone/ViewController.swift
@@ -34,7 +34,21 @@ class ViewController: UITableViewController {
                 guard let data = data else { return }
                 do {
                     let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                    print(json)
+                    guard let bookDictionaries = json as? [[String: Any]] else { return }
+
+                    self.books = []
+                    for bookDictionary in bookDictionaries {
+                        if let title = bookDictionary["title"] as? String,
+                                let author = bookDictionary["author"] as? String {
+                            let book = Book(title: title, author: author,
+                                    image: #imageLiteral(resourceName:"steve_jobs"), pages: [])
+                            self.books?.append(book)
+                        }
+                    }
+
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
                 } catch let jsonError {
                     print("Failed to parse JSON properly", jsonError)
                 }
